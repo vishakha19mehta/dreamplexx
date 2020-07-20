0bafdad7f071a748fa5eb94c83f5db96a545d85d
vishakha19mehta <45995080+vishakha19mehta@users.noreply.github.com>
2020-07-20 12:24:42 +0700

    Updates

---
+++ precache-manifest.cfeb8b9c3b4e292d3c260a36c82fab33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbbbb4efc180ed7000ce9066aaaecadb",
+    "url": "/dreamplexx/index.html"
+  },
+  {
+    "revision": "4de7eaa8c9cfd7616b56",
+    "url": "/dreamplexx/static/css/main.dc86ec91.chunk.css"
+  },
+  {
+    "revision": "44dacd7f6d1947f17033",
+    "url": "/dreamplexx/static/js/2.f5806803.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dreamplexx/static/js/2.f5806803.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4de7eaa8c9cfd7616b56",
+    "url": "/dreamplexx/static/js/main.bae0bc88.chunk.js"
+  },
+  {
+    "revision": "10510c7920fbeaa782f6",
+    "url": "/dreamplexx/static/js/runtime-main.1951562c.js"
+  }
+]);
